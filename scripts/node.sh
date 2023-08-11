f797ae6428212953f39b653bda31c2244fcb3603
Nir Tamir <nirtamir2@gmail.com>
2023-08-11 03:16:44 +0300

    use volta again (fix something with .profile missing node in obsidian plugin slidev)

--- scripts/node.sh
+++ scripts/node.sh
@@ -12,11 +12,27 @@ CONFIG_DIR="$ROOT_DIR/config"
 # *************************************
 # node
 #
-printf "installed fnm"
-brew install fnm
-fnm install --lts
-fnm use lts
 
+echo "installing node and yarn via volta"
+curl https://get.volta.sh | bash | volta install node yarn
+printf "installed volta\n"
+#
+## loading nvm for now
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+#
+#if [ -L "$NVM_DIR/default-packages" ]; then
+#  echo "found nvm default-packages - removing..."
+#  rm "$NVM_DIR/default-packages"
+#fi
+#
+#echo "linking $NVM_DIR/default-packages"
+#ln -s "$CONFIG_DIR/package-lists/nvm-default-packages.txt" "$NVM_DIR/default-packages"
+#
+#nvm install stable
+#nvm alias default stable
+#nvm use stable
+#
 printf "updating npm\n"
 npm i -g npm
 
