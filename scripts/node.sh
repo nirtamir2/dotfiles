6a59ab9bbc574063d68fdc5a32fe8d0911057b3d
Nir Tamir <nirtamir2@gmail.com>
2023-09-07 22:08:17 +0300

    node installs with pnpm

--- scripts/node.sh
+++ scripts/node.sh
@@ -24,33 +24,37 @@ npm i -g npm
 printf "installing pnpm"
 npm install -g pnpm
 
+# https://pnpm.io/completion
+printf "installing pnpm completions"
+pnpm install-completion zsh
+
 printf "installing switch-branch-cli"
-npm install -g switch-branch-cli
+pnpm install -g switch-branch-cli
 
 printf "installing jiragit"
-npm install -g @nirtamir2/jiragit
+pnpm install -g @nirtamir2/jiragit
 
 printf "installing svg-term-cli - Share terminal sessions as razor-sharp animated SVG everywhere"
-npm install -g svg-term-cli
+pnpm install -g svg-term-cli
 
 printf "installing npkill to remove node_modules"
-npm i -g npkill
+pnpm i -g npkill
 
 printf "installing kill-port to kill open port"
-npm i -g kill-port
+pnpm i -g kill-port
 
 printf "Installing git change date"
-npm i -g git-change-date
+pnpm i -g git-change-date
 
 # https://github.com/danvk/source-map-explorer
 printf "Installing source-map-explorer"
-npm i -g source-map-explorer
+pnpm i -g source-map-explorer
 
 # https://github.com/antfu/ni - Use the right package manager
-npm i -g @antfu/ni
+pnpm i -g @antfu/ni
 
 # https://github.com/wclr/yalc better npm link - publish packages locally
-npm i -g yalc
+pnpm i -g yalc
 
 # Download templates and git repositories https://github.com/unjs/giget
-npm i -g giget
+pnpm i -g giget
