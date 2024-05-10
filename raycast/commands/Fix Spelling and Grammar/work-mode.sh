819e9e7db216cc243da891752c34b601c7b47db6
Nir Tamir <nirtamir2@gmail.com>
2024-05-10 13:58:41 +0300

    work script

---
+++ raycast/commands/Fix Spelling and Grammar/work-mode.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Work Mode
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Open work apps
+# @raycast.author Nir Tamir
+# @raycast.authorURL https://nirtamir.com
+
+webstorm ~/dev/news-spectator/nspec
+open "/Applications/Warp.app"
+open "/Applications/Clockify Desktop.app"
+open "/Applications/Microsoft Teams (work or school).app"
+open "/Applications/Arc.app"
