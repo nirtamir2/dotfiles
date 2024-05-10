d8eb4e857589d528e7e2709e93bcceadc4170c16
Nir Tamir <nirtamir2@gmail.com>
2024-05-10 13:01:42 +0300

    open dotfiles raycast script

---
+++ raycast/commands/Fix Spelling and Grammar/open-dotfiles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open dotfiles
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Open dotfiles in WebStorm
+# @raycast.author Nir Tamir
+# @raycast.authorURL https://nirtamir.com
+
+webstorm ~/dotfiles
+
