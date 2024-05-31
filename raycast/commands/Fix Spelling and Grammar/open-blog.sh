e1fcad2110c82c902b2ba612302bad6ff69c611f
Nir Tamir <nirtamir2@gmail.com>
2024-06-01 01:32:55 +0300

    open blog

---
+++ raycast/commands/Fix Spelling and Grammar/open-blog.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open blog
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Open blog
+# @raycast.author Nir Tamir
+# @raycast.authorURL https://nirtamir.com
+
+webstorm ~/dev/work/blogs/blog
+
