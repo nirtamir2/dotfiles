8530002efc38485d9f6a50ecb8d978ac11d89ed2
Nir Tamir <nirtamir2@gmail.com>
2024-10-11 12:02:01 +0300

    open eslint config

---
+++ raycast/commands/Fix Spelling and Grammar/open-eslint-config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open ESLint config
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Open ESLint config in Webstorm
+# @raycast.author Nir Tamir
+# @raycast.authorURL https://nirtamir.com
+
+webstorm ~/dev/work/eslint-config
+
